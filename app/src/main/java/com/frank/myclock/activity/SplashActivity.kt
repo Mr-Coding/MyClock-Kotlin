3481248d68d2f90faab56ebb9efedde5fe08fdcf
frank <frank.code@outlook.com>
2020-01-28 17:10:43 +0800

    v 0.27.0 武汉加油！中国加油

--- app/src/main/java/com/frank/myclock/activity/SplashActivity.kt
+++ app/src/main/java/com/frank/myclock/activity/SplashActivity.kt
@@ -1,18 +1,32 @@
 package com.frank.myclock.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
 import com.frank.myclock.R
 
 class SplashActivity : AppCompatActivity() {
+    private val context = this
+    private val thread  = object : Thread() {
+        override fun run() {
+            super.run()
+            try {
+                sleep(3000)
+                val intent = Intent(applicationContext,MainActivity::class.java)
+                context.startActivity(intent)
+                context.finish()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_splash)
+
+        thread.start()
     }
 }
